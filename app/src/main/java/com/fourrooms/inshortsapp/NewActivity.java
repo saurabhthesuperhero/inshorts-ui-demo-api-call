14875652d0e687eebbd984c48861cf3e3f974018
saurabhthesuperhero <2021Momisgoid1234$>
2022-09-28 19:25:43 +0530

    stop handler for scrol and srarr tooooooooooo

--- app/src/main/java/com/fourrooms/inshortsapp/NewActivity.java
+++ app/src/main/java/com/fourrooms/inshortsapp/NewActivity.java
@@ -2,8 +2,11 @@ package com.fourrooms.inshortsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import com.fourrooms.inshortsapp.adapter.ViewPagerAdapter;
 import com.fourrooms.inshortsapp.databinding.ActivityMainBinding;
@@ -16,7 +19,7 @@ public class NewActivity extends AppCompatActivity {
     ArrayList<LocalTechModel> dataArrayList = new ArrayList<>();
     public ViewPagerAdapter adapter;
     public ActivityNewBinding binding;
-
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +29,38 @@ public class NewActivity extends AppCompatActivity {
         binding.viewPagerMain.setAdapter(adapter);
 
         setDemoData();
+        setBehaviour();
+    }
+
+    private void setBehaviour() {
+        handler= new Handler();
+        binding.viewPagerMain.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.e("checkmes", "position ,total " + position+" "+dataArrayList.size());
+                handler.removeCallbacksAndMessages(null);
+                if (!(position >=dataArrayList.size())) {
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            binding.viewPagerMain.setCurrentItem(position + 1);
+
+                        }
+                    }, 3000);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     private void setDemoData() {
@@ -36,5 +71,6 @@ public class NewActivity extends AppCompatActivity {
         dataArrayList.add(new LocalTechModel("fullscreenad","https://tamilian.to/wp-content/uploads/2021/07/Haseen-Dillruba-poster.jpg","hello hunny bunny 4","https://tamilian.to/wp-content/uploads/2021/07/Haseen-Dillruba-poster.jpg",1));
         dataArrayList.add(new LocalTechModel("data","https://sample-videos.com/img/Sample-jpg-image-50kb.jpg","hello hunny bunny 5","https://sample-videos.com/img/Sample-jpg-image-50kb.jpg",1));
         adapter.notifyDataSetChanged();
+
     }
 }
